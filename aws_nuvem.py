ec43098940ff445aab81d79af1d41623bbf03b95
Bessean <55710829+Bessean@users.noreply.github.com>
2021-05-06 22:58:31 -0300

    Add files via upload

---
+++ aws_nuvem.py
@@ -0,0 +1,36 @@
+#Objetivo
+#Inserir dados de arquivos .csv em um banco de dados relacional.
+#Extrair dados de um banco de dados relacional e gravar no formato .csv.
+
+import sqlalchemy, pandas as pd,pyodbc
+server = 'sql20211.cp1ri5jswsct.us-east-1.rds.amazonaws.com'  
+database = 'GRUPOC' 
+username = 'aluno'
+password = 'aluno123' 
+con1 = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC Driver 17 for SQL Server"
+engine = sqlalchemy.create_engine(con1)
+engine.connect()
+
+df_aluno = pd.read_csv(filepath_or_buffer = '/content/ALUNO.csv',delimiter=';',encoding = 'ISO-8859-1')
+df_convidado = pd.read_csv(filepath_or_buffer = '/content/CONVIDADO.csv',delimiter=';',encoding = 'ISO-8859-1')
+df_fregues = pd.read_csv(filepath_or_buffer = '/content/FREGUES.csv',delimiter=';',encoding = 'ISO-8859-1')
+df_funcionario = pd.read_csv(filepath_or_buffer = '/content/FUNCIONARIO.csv',delimiter=';',encoding = 'ISO-8859-1')
+df_imovel = pd.read_csv(filepath_or_buffer = '/content/IMOVEL.csv',delimiter=';',encoding = 'ISO-8859-1')
+
+df_aluno.to_sql(("aluno"),engine,if_exists = "replace")
+df_convidado.to_sql(("convidado"),engine,if_exists = "replace")
+df_fregues.to_sql(("fregues"),engine,if_exists = "replace")
+df_funcionario.to_sql(("funcionario"),engine,if_exists = "replace")
+df_imovel.to_sql(("imovel"),engine,if_exists = "replace")
+
+import sqlalchemy, pandas as pd,pyodbc
+server = 'sql20211.cp1ri5jswsct.us-east-1.rds.amazonaws.com'  
+database = 'DATAENG_AC2' 
+username = 'aluno'
+password = 'aluno123' 
+con1 = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC Driver 17 for SQL Server"
+engine2 = sqlalchemy.create_engine(con1)
+engine2.connect()
+
+df_informacao = pd.read_sql(sql ="SELECT * FROM INFORMACAO",con=engine2)
+df_informacao.to_csv(("informação_ac2.csv"),index=False, line_terminator='\n')
